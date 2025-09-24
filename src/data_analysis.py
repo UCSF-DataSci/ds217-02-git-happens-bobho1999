f07b66d21e6be5591684f8e20731796bd29e3f9e
Bob Ho <28524055+bobho1999@users.noreply.github.com>
2025-09-24 14:26:29 -0700

    Add skeleton function in data_analysis.py

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -1,1 +1,23 @@
 TODO
+
+# Read CSV and return list of student data
+def load_students(filename):
+    pass
+
+# Calculate and return average
+def calculate_average_grade(students): 
+    pass
+
+# Count students in Math
+def count_math_students(students):
+    pass
+
+# Create formatted report string
+def generate_report():
+    pass
+
+# Write report to file
+def save_report(report, filename):
+
+if name == “main”:
+    print("Hi")
