c063a5bc5a1370c0b5b4e5b8c161786f7514ea97
Bob Ho <28524055+bobho1999@users.noreply.github.com>
2025-09-24 13:55:58 -0700

    Add more echo message to provide feedback

--- setup_project.sh
+++ setup_project.sh
@@ -6,17 +6,21 @@ mkdir -p src data output
 echo "Directories created"
 
 # Create files using "here-documents"
+echo "Setting up .gitignore"
 cat > .gitignore << 'EOF'
 EOF
 
+echo "Setting up student.csv"
 cat > data/student.csv << 'EOF'
 name,age,grade,subject
 EOF
 
+echo "Setting up data_analysis.py"
 cat > src/data_analysis.py << 'EOF'
 TODO
 EOF
 
+echo "Setting up data_analysis_functions/py"
 cat > src/data_analysis_functions.py << 'EOF'
 TODO
 EOF
