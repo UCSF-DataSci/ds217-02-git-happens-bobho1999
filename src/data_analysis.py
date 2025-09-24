6d9ceae7bf4b773f834231bc359754ba97fa0194
Bob Ho <28524055+bobho1999@users.noreply.github.com>
2025-09-24 14:38:26 -0700

    Implement calculate_average_grade

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -4,11 +4,17 @@
 def load_students(filename):
     with open(filename, 'r') as file:
         content = file.readlines()[1:]
-        print(f"File content: {content}")
+        return content
 
 # Calculate and return average
 def calculate_average_grade(students): 
-    pass
+    sum = 0
+    for s in students:
+        score = int(s.strip().split(',')[2])
+        sum += score
+    avg = sum/len(students)
+    return avg
+
 
 # Count students in Math
 def count_math_students(students):
@@ -24,4 +30,10 @@ def save_report(report, filename):
 
 if __name__ == "__main__":
     print("Hi")
-    load_students("data/students.csv")
+
+    contents = load_students("data/students.csv")
+    print(f"File content: {contents}")
+
+    avg = calculate_average_grade(contents)
+    print(f"{avg:.1f}")
+
