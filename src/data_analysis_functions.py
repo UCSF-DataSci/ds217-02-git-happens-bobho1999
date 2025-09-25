7e7a0c14065456eb76f7818f5b27661dd0634df1
Bob Ho <28524055+bobho1999@users.noreply.github.com>
2025-09-25 00:41:47 -0700

    Implement load_data, load csv

--- src/data_analysis_functions.py
+++ src/data_analysis_functions.py
@@ -2,10 +2,28 @@
 
 # Generic loader that checks file extension
 def load_data(filename): 
-    pass 
+    # Check for
+    if filename.endswith(".csv"):
+        print("Processing csv file")
+        return load_csv(filename)
+    else:
+        print("Cannot load file. File must be in csv format")
+
 
 def load_csv(filename): # Load CSV data (same technique as basic script)
-    pass
+    with open(filename, 'r') as file:
+        content = file.readlines()[1:]
+
+    # Initialized an empty list to store student data
+    students = []
+
+    # Iterate through list "content" and split each element. 
+    # Store each student data in a dictionary and append into the students list
+    for s in content:
+        temp = s.strip().split(',')
+        students.append({"name": temp[0], "age": int(temp[1]), "grade": int(temp[2]), "subject": temp[3]})
+        
+    return students
 
 def analyze_data(students): # Return dictionary with multiple statistics
     pass
@@ -17,7 +35,8 @@ def save_results(results, filename): # Save detailed report
     pass
 
 def main(): # Orchestrate the analysis using all functions
-    pass
+    data = load_data("data/students.csv")
+
 
 if __name__ == "__main__":
     main()
