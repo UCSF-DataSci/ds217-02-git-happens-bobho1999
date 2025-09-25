aff7ef56e098079210214549be76ebef315a92a7
Bob Ho <28524055+bobho1999@users.noreply.github.com>
2025-09-24 23:35:08 -0700

    -Implement count_math_student -Add main

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -18,22 +18,30 @@ def calculate_average_grade(students):
 
 # Count students in Math
 def count_math_students(students):
-    pass
+    count = 0
+    for s in students:
+        subject = s.strip().split(',')[3]
+        if subject == "Math":
+            count += 1
+    return count
+
 
 # Create formatted report string
-def generate_report():
-    pass
+def generate_report(average, math_count):
+    print(f"Class average: {average:.1f}")
+    print(f"Number of Math student: {math_count}")
 
 # Write report to file
 def save_report(report, filename):
     pass
 
-if __name__ == "__main__":
-    print("Hi")
-
+def main():
     contents = load_students("data/students.csv")
-    print(f"File content: {contents}")
-
+    # print(f"File content: {contents}")
     avg = calculate_average_grade(contents)
-    print(f"{avg:.1f}")
+    math_count = count_math_students(contents)
+    generate_report(avg, math_count)
+    save_report(report, "output/analysis_report.txt")
 
+if __name__ == "__main__":
+    main()
